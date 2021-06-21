6909b4c7f9eb834c87b334c9ccf4d9b8faaf97b0
indranasaha <indranasaha@yahoo.in>
2021-06-21 17:23:54 +0530

    first commit

--- src/test/java/com/learning/SelTest/SampleTest.java
+++ src/test/java/com/learning/SelTest/SampleTest.java
@@ -18,7 +18,7 @@ public class SampleTest {
    driver=new ChromeDriver(); 
 //   Comment added- 19 June added
      
-      // Launch Website  
+      // Launch of yahoo Website here 21/06/2021 
    driver.navigate().to("https://in.yahoo.com/");  
      
     
